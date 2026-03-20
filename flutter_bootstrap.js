444ec7ac7aff7d3237f5c357044a68a89a8254bc
MATHURADA MEEPAN <165973996+mookmathu@users.noreply.github.com>
2026-03-20 20:18:23 +0700

    fix firebase web final

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1054480554"
+    serviceWorkerVersion: "4043982946"
   }
 });
