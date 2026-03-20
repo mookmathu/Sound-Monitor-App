8609e0ce002a4b249fe3e563251bc93f744a9b34
MATHURADA MEEPAN <165973996+mookmathu@users.noreply.github.com>
2026-03-20 20:06:20 +0700

    fix base path

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3474124392"
+    serviceWorkerVersion: "2937063526"
   }
 });
